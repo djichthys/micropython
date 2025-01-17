7d4925659cecec9891f57743befdb0b9df302ac9
Dejice Jacob <dejice.jacob@glasgow.ac.uk>
2025-01-17 16:49:57 +0000

    partial script

--- ports/sonata-rv32e/perf_bench/benchrun.py
+++ ports/sonata-rv32e/perf_bench/benchrun.py
@@ -1,8 +1,34 @@
+#!/usr/bin/python3 
+
+bm_params = { 
+  "bm_chaos": {
+                (100, 50): (0.25, 100, 50, 50, 50, 1234),
+                (1000, 1000): (0.25, 200, 400, 400, 1000, 1234),
+                (5000, 1000): (0.25, 400, 500, 500, 7000, 1234),
+              },
+  "bm_fannkuch": {
+                (32, 10): (3,),
+                (50, 10): (5,),
+                (100, 10): (6,),
+                (500, 10): (7,),
+                (1000, 10): (8,),
+                (5000, 10): (9,),
+              }, 
+  "bm_fft": {
+                (50, 25): (2, 128),
+                (100, 100): (3, 256),
+                (1000, 1000): (20, 512),
+                (5000, 1000): (100, 512),
+            }, 
+  "bm_nqueens": 
 
 
+
+
+              
+}
 
 def bm_run(N, M):
-
     # Pick sensible parameters given N, M
     cur_nm = (0, 0)
     param = None
@@ -13,7 +39,6 @@ def bm_run(N, M):
     if param is None:
         print(-1, -1, "SKIP: no matching params")
         return
-
     # Run and time benchmark
     run, result = bm_setup(param)
     run()
@@ -21,5 +46,6 @@ def bm_run(N, M):
     print(norm, out)
 
 
-arg_n, arg_m = 
-bm_run(arg_n, arg_m)
+if __name__ == '__main__': 
+    for bm in bm_params.keys(): 
+      pr
